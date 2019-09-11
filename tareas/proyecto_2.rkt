2951f0befc7bc3438588fcfff9e99763b185369f
Sergio <a01365942@itesm.mx>
2019-09-11 09:53:40 -0500

    Primer Problema resuelto de Proyecto 2 :D

---
+++ tareas/proyecto_2.rkt
@@ -0,0 +1,54 @@
+
+
+#lang racket
+
+#|
+Hacer las siguientes tres funciones recursivas:
+
+Reverse múltiple nivel: la función recibe una lista y regresa la lista con sus elementos al revés, pero a cualquier nivel, es decir, si uno de los elementos de la lista es una lista, dicha lista también se debe entregar al revés.
+Palíndromo: la función recibe una lista de símbolos y regresa TRUE, si la lista es un palíndromo y FALSE en caso contrario.
+Cuenta vocales: la función recibe una lista de caracteres y regresa el número de vocales que contiene.
+
+|#
+
+;(define f '(3 4 5))
+
+#|(define (reverse-multiple l)
+
+  (if (null? l)
+          '()
+          (if (list? (car l))
+              (append (cdr l) (list(reverse-multiple (car l))))
+              (append (reverse-multiple (cdr l)) (list(car l)) )
+          )
+          
+  )
+   
+)|#
+
+(define (reverse-multiple l)
+
+  (if (null? l)
+          '()
+          (if (list? (car l))
+              (append (reverse-multiple(cdr l)) (list(reverse-multiple (car l))))
+              (append (reverse-multiple (cdr l)) (list(car l)) )
+          )
+          
+  )
+   
+)
+
+(reverse-multiple '(5 (12 78 34) 23 67 3 (7 9 8)))
+
+(define l '(5 (3 4) 7)
+  )
+
+(list? '())
+
+(if (list? (car l))
+    "Es lista"
+    "No es lista"
+)
+
+(car (cdr l))
