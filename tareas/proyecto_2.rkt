f88efe9d7749f0a06ab13e13548636065744c405
Sergio <a01365942@itesm.mx>
2019-09-11 11:55:41 -0500

    El proyecto 2 se ajusto para su entrega final

--- tareas/proyecto_2.rkt
+++ tareas/proyecto_2.rkt
@@ -1,67 +1,34 @@
-
-
 #lang racket
 
 #|
-Hacer las siguientes tres funciones recursivas:
-
-Reverse múltiple nivel: la función recibe una lista y regresa la lista con sus elementos al revés, pero a cualquier nivel, es decir, si uno de los elementos de la lista es una lista, dicha lista también se debe entregar al revés.
-Palíndromo: la función recibe una lista de símbolos y regresa TRUE, si la lista es un palíndromo y FALSE en caso contrario.
-Cuenta vocales: la función recibe una lista de caracteres y regresa el número de vocales que contiene.
-
+Reverse múltiple nivel: la función recibe una lista y regresa la lista con sus elementos
+al revés, pero a cualquier nivel, es decir, si uno de los elementos de la lista es una
+lista, dicha lista también se debe entregar al revés.
 |#
-
-;(define f '(3 4 5))
-
-#|(define (reverse-multiple l)
-
-  (if (null? l)
+(define (reverse-multiple lista)
+  
+  (if (null? lista)
           '()
-          (if (list? (car l))
-              (append (cdr l) (list(reverse-multiple (car l))))
-              (append (reverse-multiple (cdr l)) (list(car l)) )
-          )
-          
-  )
-   
-)|#
-
-(define (reverse-multiple l)
-
-  (if (null? l)
-          '()
-          (if (list? (car l))
-              (append (reverse-multiple(cdr l)) (list(reverse-multiple (car l))))
-              (append (reverse-multiple (cdr l)) (list(car l)) )
-          )
-          
+          (if (list? (car lista))
+              (append (reverse-multiple(cdr lista)) (list(reverse-multiple (car lista))))
+              (append (reverse-multiple (cdr lista)) (list(car lista)) )
+          )        
   )
    
 )
 
-;(reverse-multiple '(5 (12 78 34) 23 67 3 (7 9 8)))
-
-(define l '(5 (3 4) 7)
-  )
-
-;(list? '())
-
-
+;Palíndromo: la función recibe una lista de símbolos y regresa TRUE, si la lista es un palíndromo y FALSE en caso contrario.
 (define (palindromo lista)
   (if (or (null? lista) (<= (length lista) 1) )
       #t
-      (if (equal? (car lista) (car(reverse (cdr lista))))
-          (palindromo (cdr(reverse (cdr lista) )) )
+      (if (equal? (car lista) (car(reverse-multiple (cdr lista))))
+          (palindromo (cdr(reverse-multiple (cdr lista) )) )
           #f
       )
    )
 )
 
-(palindromo '(a a b c b a a))
-(palindromo '(a a b c c b a a))
-(palindromo '(a a b c x t c b a a))
-
-
+;Cuenta vocales: la función recibe una lista de caracteres y regresa el número de vocales que contiene.
 (define (vocales lista)
   (if (null? lista)
       0
@@ -75,7 +42,8 @@ Cuenta vocales: la función recibe una lista de caracteres y regresa el número 
        )
   )
 )
-
-(vocales '(a e i % o ? 4 5 u a o))
-(vocales '(a e i % o ? 4 5 u g r i t a o))
+(reverse-multiple '(1 2 3 (a b c) 4)) 
+(palindromo '(a a b c b a a))
+(palindromo '(a a b c c b a a))
+(palindromo '(a a b c c a a))
 (vocales '(1 a 2 b c % e 4 u))
